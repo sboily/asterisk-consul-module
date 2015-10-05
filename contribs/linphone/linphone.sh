d3d6bcf1bfc33b816ecaf9d203aba2bed84e41d8
Sylvain Boily <sboily@avencall.com>
2015-10-05 18:01:16 -0400

    add a phone on docker-compose

--- contribs/linphone/linphone.sh
+++ contribs/linphone/linphone.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
 pulseaudio --system &> /tmp/pulse.log &
-linphonec $@
+echo "Init linphone daemon"
+linphonecsh init -a
+sleep 1
+echo "Register linphone daemon"
+linphonecsh register --host asterisk --username sylvain --password sylvain
+
+while true
+do
+    echo "Telecom is fun..."
+    sleep 30
+done
